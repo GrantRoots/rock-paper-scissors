d8def92e049e96a28101161463e41a7de9bebb6b
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-06-27 20:03:41 -0700

    Make getHumanChoice and playRound
    I made the function for getHumanChoice to get an answer from the human using prompt and made sure that it had to be either rock paper or scissors. I also made the function playRound to see who won the round.

--- script.js
+++ script.js
@@ -14,6 +14,48 @@ function getComputerChoice() {
     }
 }
 
-let computer_choice = getComputerChoice()
+function getHumanChoice() {
+    let choice = prompt('Rock Paper or Scissors?').toLowerCase();
 
-console.log(computer_choice)
+    if (choice != 'rock' && choice != 'paper' && choice != 'scissors') {
+        choice = getHumanChoice();
+    }
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('You lose! Paper beats Rock');
+        computerChoice++;
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('You lose! Scissors beats Paper');
+        computerChoice++;
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('You lose! Rock beats Scissors');
+        computerChoice++;
+    }
+    else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You win! Rock beats Scissors');
+        humanChoice++;
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You win! Paper beats Rock');
+        humanChoice++;
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You win! Scissors beats Paper');
+        humanChoice++;
+    }
+}
+
+let computerChoice = getComputerChoice()
+
+let humanChoice = getHumanChoice()
+
+console.log(computerChoice)
+console.log(humanChoice)
+
+let computerScore = 0
+let humanScore = 0
